df6c56a81c3cdddd75a189b8080a6933b3989019
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-09-26 15:46:33 -0700

    Creando clase VideoPost

---
+++ lib/domain/entities/video_post.dart
@@ -0,0 +1,12 @@
+class VideoPost {
+  final String caption;
+  final String videoUrl;
+  final int like;
+  final int views;
+
+  VideoPost(
+      {required this.caption,
+      required this.videoUrl,
+      this.like = 0,
+      this.views = 0});
+}
