971745263f71e8a31cbf2bace0ea11b8758ca3b0
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-09-26 16:21:12 -0700

    Map con las propiedades y localización de videos

---
+++ lib/shared/data/local_video_post.dart
@@ -0,0 +1,50 @@
+List<Map<String, dynamic>> VideoPost = [
+  {
+    'name': 'Subiendo escaleras automáticas',
+    'videoUrl': 'assets/videos/1.mp4',
+    'likes': '200',
+    'views': '18797533',
+  },
+  {
+    'name': 'Planta apreciada por peatones',
+    'videoUrl': 'assets/videos/2.mp4',
+    'likes': '400000',
+    'views': '1000033000',
+  },
+  {
+    'name': 'Que borroso se ve todo!',
+    'videoUrl': 'assets/videos/3.mp4',
+    'likes': '9887',
+    'views': '800',
+  },
+  {
+    'name': '¿Esto es trigo? Que interesante se ve',
+    'videoUrl': 'assets/videos/4.mp4',
+    'likes': '343',
+    'views': '22424',
+  },
+  {
+    'name': 'Esta enfermedad no me afecta a mi',
+    'videoUrl': 'asssets/videos/5.mp4',
+    'likes': '33943',
+    'views': '3235342',
+  },
+  {
+    'name': 'No quiero ir a trabajar hoy Mr. White',
+    'videoUrl': 'assses/videos/6.mp4',
+    'likes': '2323243',
+    'views': '5565654',
+  },
+  {
+    'name': 'Limpiar nunca fue tan divertido',
+    'videoUrl': 'assets/videos/7.mp4',
+    'likes': '737673',
+    'views': '899',
+  },
+  {
+    'name': '¿Ya llegamos a la India? mmm Sí',
+    'videoUrl': 'asssets/videos/8.mp4',
+    'likes': '8987',
+    'views': '82384',
+  },
+];
